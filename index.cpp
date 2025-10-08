37d515bbe4b898af78e10fde404dcafccceb934b
mishanyya <mishanyakashin@gmail.com>
2025-10-08 16:23:03 +0300

    добавил способ компилирования и запуска программы через терминал 081025

--- index.cpp
+++ index.cpp
@@ -1,3 +1,16 @@
+/*
+
+//1 установить компилятор через терминал
+//sudo apt install g++
+//2 в терминале зайти в папку Programm_on_clear_C++ с этим файлом
+//cd /home/mishanyya/MyCodes/Programm_on_clear_C++/ 
+//3 скомпилировать файл index.cpp в файл index
+//g++ index.cpp -o index
+//4 в папке Programm_on_clear_C++ должен появиться исполняемый файл index
+//5 запустить файл на выполнение в терминале
+//./index*/
+
+
 #include <iostream>
 #include <cstdlib> 
 using namespace std;
